f1fdbf2c08bbc01c16a41d410553e199ec55542a
Jacob <jacobrobinsoncoleman@gmail.com>
2023-03-02 14:33:30 -0500

    made 1 more duck script, going insane using python

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # CyberFinal
 This is for AMSA Senior year cyber, focus on rubber ducky attacks
-test test kuang
--- defend/mainDefence.py
+++ defend/mainDefence.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
-import sys
-import usb.core
-# find USB devices
-dev = usb.core.find(find_all=True)
-# loop through devices, printing vendor and product ids in decimal and hex
-for cfg in dev:
-  sys.stdout.write('Decimal VendorID=' + str(cfg.idVendor) + ' & ProductID=' + str(cfg.idProduct) + '\n')
-  sys.stdout.write('Hexadecimal VendorID=' + hex(cfg.idVendor) + ' & ProductID=' + hex(cfg.idProduct) + '\n\n')
+# credits to, i stole this code on stack overflow nosklo
+# https://stackoverflow.com/questions/53913408/how-to-disable-enable-a-specific-usb-port-in-windows-with-python
+
+import subprocess
+# Fetches the list of all usb devices:
+result = subprocess.run(['devcon', 'hwids', '=usb'], 
+    capture_output=True, text=True)
+
+# ... add code to parse the result and get the hwid of the device you want ...
+
+subprocess.run(['devcon', 'disable', 'HID\VID_03EB&PID_2401&REV_0100']) # to disable
