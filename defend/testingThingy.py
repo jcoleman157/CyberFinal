7a10dd8b3cff8232a332548e7cc42a4cbd9ee3d1
Jacob <jacobrobinsoncoleman@gmail.com>
2023-05-09 11:29:08 -0400

    Kind of nuked the old code and made it work (somewhat)

--- defend/testingThingy.py
+++ defend/testingThingy.py
@@ -1,9 +1,28 @@
 import subprocess
+import time
 
-# Define the command you want to execute
-cmd_code = '"C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\devcon" hwids *hid*keyboard* > "C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\hwid_trust.txt"'
+def check():
+    # Define the command you want to execute
+    cmd_code = '"C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\devcon" hwids *hid*keyboard* > "C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\hwid_trust.txt"'
 
-# output = subprocess.Popen(cmd_code)
+    # output = subprocess.Popen(cmd_code)
 
-subprocess.run(cmd_code, shell=True)
- 
+    subprocess.run(cmd_code, shell=True)
+
+
+
+def disableHID():
+    dis_code = '"C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\devcon" disable *hid*keyboard*'
+    subprocess.run(dis_code, shell= True)
+    print("Bye-Bye silly Keyboards \n")
+
+disableHID()
+
+def enableHID():
+    en_code = '"C:\\Users\\JColeman2023\\OneDrive - amsacs.org\\Documents\\rubberDuckyShnanaganery\\CyberFinal\\defend\\devcon" enable *hid*keyboard*'
+    subprocess.run(en_code, shell=True)
+    print("We back with da keyboards \n")
+
+time.sleep(10) #snoozez the program
+
+enableHID()
