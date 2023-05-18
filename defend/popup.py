48abdcc5be722b849b21b61ace24dae6fe5c051b
AlexK <alexkuang0314@gmail.com>
2023-05-18 12:55:40 -0400

    tried to make popup work

--- defend/popup.py
+++ defend/popup.py
@@ -1,24 +1,30 @@
+#not done yet + doesn't work :(((
 from tkinter import *
 from tkinter import ttk
-#root = Tk()
-#root.title("asdf")
-#frm = ttk.Frame(root, padding=100)#frame padding str with vals for left, top, right, bottom
-#frm.grid()
-#ttk.Label(frm, text="you're mom").grid(column=0, row=0)
-#ttk.Button(frm, text=":(", command=root.destroy).grid(column=0, row=1)
-#root.mainloop()
 
-#Create an instance of Tkinter frame
-win= Tk()
-#Define the geometry of the function
-win.geometry("750x250")
-def close_win():
-   win.destroy()
+win= Tk() #Create instance of Tkinter frame
+win.attributes('-fullscreen',True)
+frame = ttk.Frame(win, padding=0)
+frame.grid()
+text0 = ttk.Label(frame, text="New device detected!").grid(row=0, column=0, padx=100, pady=100)
+text1 = ttk.Label(frame, text="Do you trust this device?").grid(row=1, column=0, padx=100, pady=100)
+
+
 def disable_event():
    pass
-#Create a button to close the window
-#btn = ttk.Button(win, text ="Click here to Close",command=close_win)
-#btn.pack()
+
+trust = False
+
+def cursedCode(bool):
+  if bool == False:
+    bool = True
+
+btn1 = ttk.Button(win, text ="Do Not Trust", command=win.destroy()).grid(row=2, column=0, padx=100, pady=100)#button for untrusted devices (closes the window as well)
+btn1.pack()
+
+btn2 = ttk.Button(win, text = "Trust", command = lambda:[cursedCode(trust), win.destroy()]).grid(row=3, column=0, padx=100, pady=100)
+btn2.pack()
+
 #Disable the Close Window Control Icon
 win.protocol("WM_DELETE_WINDOW", disable_event)
 win.mainloop()
